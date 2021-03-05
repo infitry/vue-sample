24409ec9a2990555f7eec6c475c9c57d726d809c
leesw@infobank.net <leesw@infobank.net>
2021-03-05 16:26:21 +0900

    axios 예제 추가

--- src/components/pages/http-axios.vue
+++ src/components/pages/http-axios.vue
@@ -21,10 +21,19 @@ export default {
     getData() {
       const th = this
       this.$axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
-        .then(function(response) {
+        .then(function(response) { // 성공 시 처리
           console.log(response)
           th.dataList = response.data
         }) 
+        .catch(function (error) { // 에러처리
+          if (error.response) {
+            // 요청 성공했으나 서버가 2xx의 범위를 벗어나는 상태 코드로 응답
+          } else if (error.request) {
+            // 요청이 이루어졌으나 응답을 받지 못함
+          } else {
+            // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생하였습니다.
+          }
+        })
     }
   }
 }
